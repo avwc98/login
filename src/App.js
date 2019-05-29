a356aea8e2560cae67151032c65b97e637491761
avwc98 <i201621048@cibertec.edu.pe>
2019-05-29 18:22:04 -0500

    shopping_history

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import '../src/assets/font-awesome/css/font-awesome.min.css';
 import '../src/assets/css/dataTables.bootstrap4.min.css'
 import '../src/index.css';
 import Select from 'react-select';
+
 import logo_user from '../src/img/user-icon.jpg';
 import {
     Button,
@@ -26,6 +27,7 @@ import ReactTable from "react-table";
 import swal from 'sweetalert'
 import './App.css';
 import ModalFooter from "reactstrap/es/ModalFooter";
+import Table from "reactstrap/es/Table";
 
 // ARREGLO PARA LA DATA DE ALGUNOS COMBOBOX
 const arr_prueba = [
@@ -43,7 +45,6 @@ class index extends Component {
             subTitle: "",
             str_prueba: ""
         }
-
     }
 
     componentDidMount() {
@@ -75,50 +76,11 @@ class index extends Component {
         const {str_prueba} = this.state;
         return (
             <div className="wrapper">
-                <nav id="sidebar">
-                    <div className="sidebar-header">
-                        <span><b>VIDEO</b>JUEGOS</span>
-                    </div>
-                    <div className="user-panel">
-                        <div className="pull-left image">
-                            <img src={logo_user} className="rounded-circle" alt=""/>
-                        </div>
-                        <div className="pull-left info">
-                            <p></p>
-                            <a href=""><i className="fa fa-circle text-success"/> Online</a>
-                        </div>
-                    </div>
-                    <div className="header"> OPCIONES</div>
-                    <ul className="list-unstyled components" id="">
-                        <a href="#" aria-expanded="false" className="dropdown-toggle">
-                            <li>COSA1</li>
-                        </a>
-                        <ul className="list-unstyled collapse">
-                            <li>SUBCOSA1<i className="fa fa-circle-o"/></li>
-                            <li>SUBCOSA2</li>
-                        </ul>
-                        <li>COSA2</li>
-                        <li>COSA3</li>
-                        <li className="close_sesion_mobile">
-                            <a className="nav-link" href="">CERRAR SESSION</a>
-                        </li>
-                    </ul>
-                </nav>
 
                 <div id="content" className="p-0">
                     <nav className="navbar navbar-expand-lg navbar-main">
                         <div className="container-fluid">
-                            <button type="button" id="sidebarCollapse" className="navbar-btn">
-                                <span/>
-                                <span/>
-                                <span/>
-                            </button>
                             <div className="collapse navbar-collapse" id="navbarSupportContent">
-                                <ul className="nav navbar-nav ml-auto">
-                                    <li className="nav-item close_sesion">
-                                        <a className="nav-link" href="">CERRAR SESSION</a>
-                                    </li>
-                                </ul>
                             </div>
                         </div>
 
@@ -132,53 +94,118 @@ class index extends Component {
 
                     <div className="box box-primary">
                         <div className="box-header with-border d-flex p-0">
-                            <h3 className="box-title p-3">Listado de CONTRATOS</h3>
+                            <h3 className="box-title p-3"><b><em>SHOPPING HISTORY</em></b></h3>
                             <ul className="nav nav-pills ml-auto p-2">
-                                <li className="nav-item">
-                                    <Button className="btn btn-primary pull-righ"
-                                            onClick={this.modalPrueba.bind(this)}>
-                                        <i className="fa fa-plus"/> Registrar</Button>
 
-                                </li>
                             </ul>
                         </div>
 
                         <div className="box-body">
+                            <div className="container">
+                                <div className="container-fluid">
 
-                            <ReactTable
-                                //data={}
-                                noDataText="Oh, no hay data"
-                                filterable
-                                columns={
-                                    [
-                                        {
-                                            Header: "PRUEBA",
-                                            columns: [
-                                                {
-                                                    Header: 'ATRIBUTO 1',
-                                                    id: "attribute01"
-                                                },
-                                                {
-                                                    Header: 'ATRIBUTO 2',
-                                                    id: "attribute02"
-                                                },
-                                                {
-                                                    Header: 'ATRIBUTO 3',
-                                                    id: "attribute03"
-                                                },
-                                                {
-                                                    Header: 'ATRIBUTO 4',
-                                                    id: "attribute04"
-                                                },
-                                                {
-                                                    Header: 'ATRIBUTO 5',
-                                                    id: "attribute05"
-                                                }
+                                    <div className="container h-100">
+                                        <div className="card-wrapper">
+                                            <div className="card fat">
+                                                <div className="card-body">
+                                                    <Table>
+                                                        <tbody>
+                                                        <tr>
+                                                            <th>ID</th>
+                                                            <td></td>
+                                                        </tr>
 
-                                            ],
-                                        },
-                                    ]
-                                }/>
+                                                        <tr>
+                                                            <th>Productos</th>
+                                                            <td></td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <th>Cantidad</th>
+                                                            <td></td>
+                                                        </tr>
+
+                                                        <tr>
+                                                            <th>Precio</th>
+                                                            <td></td>
+                                                        </tr>
+
+                                                        </tbody>
+                                                    </Table>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                    <div className="box-body">
+
+                                        <ReactTable
+                                            //data={}
+                                            noDataText="Oh, no hay data"
+                                            filterable
+                                            columns={
+                                                [
+                                                    {
+                                                        Header: "HISTORY",
+                                                        columns: [
+                                                            {
+                                                                Header: '',
+                                                                id: "accion"
+                                                            },
+                                                            {
+                                                                Header: 'ID',
+                                                                id: "attribute01"
+                                                            },
+                                                            {
+                                                                Header: 'DESCRIPCION',
+                                                                id: "attribute02"
+                                                            },
+                                                            {
+                                                                Header: 'PRICE',
+                                                                id: "attribute03"
+                                                            },
+                                                            {
+                                                                Header: 'STOCK',
+                                                                id: "attribute01"
+                                                            },
+                                                            {
+                                                                Header: 'STATUS',
+                                                                id: "attribute01"
+                                                            },
+
+                                                        ],
+                                                    },
+                                                ]
+                                            }/>
+                                    </div>
+                                    <Modal isOpen={this.state.modalPrueba} toggle={() => this.modalPrueba()}>
+                                        <ModalHeader>
+                                            <h1>Modal de prueba</h1>
+                                        </ModalHeader>
+
+                                        <ModalBody>
+                                            <FormGroup>
+                                                <Label>Atributo 1</Label>
+                                                <Input className="Select-control"/>
+                                            </FormGroup>
+
+                                            <FormGroup>
+                                                <Label>Atributo 2</Label>
+                                                <Select className="Select-control"
+                                                        placeholder='Seleccionar valor'
+                                                        value={arr_prueba.filter(options => options.value === str_prueba)}
+                                                        onChange={this.handleInputSelect.bind(this, 'str_prueba')}
+                                                        options={arr_prueba}/>
+                                            </FormGroup>
+                                        </ModalBody>
+
+                                        <ModalFooter>
+                                            <Button color="primary" width={100} height={40}> REGISTRAR </Button>
+                                        </ModalFooter>
+                                    </Modal>
+
+
+                                </div>
+                            </div>
                         </div>
 
                         <Modal isOpen={this.state.modalPrueba} toggle={() => this.modalPrueba()}>
@@ -207,6 +234,8 @@ class index extends Component {
                             </ModalFooter>
                         </Modal>
                     </div>
+
+
                     <section className="content">
                         <footer className="my-5 pt-5 text-muted text-center text-small">
                             <p className="mb-1">&copy; 2019-2020 CIBERTEC - PROY. DSW-II</p>
@@ -219,4 +248,6 @@ class index extends Component {
     };
 }
 
+
 export default index;
+
